978a540c1616404ec552b6c770941b8a280b2ec0
valiaisnotprogrammer <ValiaChernykh@yandex.ru>
2024-07-24 14:14:31 +0300

    add async engine

---
+++ src/database/database.py
@@ -0,0 +1,19 @@
+from sqlalchemy import NullPool
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+
+from settings import settings
+
+if settings.run.MODE == "TEST":
+    DATABASE_URL = settings.db.pg_test_dns
+    DATABASE_PARAMS = {"poolclass": NullPool}
+else:
+    DATABASE_URL = settings.db.pg_dns
+    DATABASE_PARAMS = {}
+
+
+engine = create_async_engine(DATABASE_URL, **DATABASE_PARAMS)
+
+async_session_maker = sessionmaker(engine,
+                                   class_=AsyncSession,
+                                    expire_on_commit=False)
