6283ecf38521419d475c3f9a6ed169fe925e0299
valiaisnotprogrammer <ValiaChernykh@yandex.ru>
2024-07-24 14:14:57 +0300

    add abstract root endpoint

--- src/main.py
+++ src/main.py
@@ -15,5 +15,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
+@app.get("/")
+async def hello():
+    return {"message": "There are no errors here"}
+
+
 if __name__ == "__main__":
     uvicorn.run(app, host=settings.run.HOST, port=settings.run.PORT)
